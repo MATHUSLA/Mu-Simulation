72d5b2ce5207f5b96c8ce26f2977a255c033ee6d
bhgomes <bhgomes.github@gmail.com>
2018-09-08 10:29:01 -0400

    try non-MT random generator

--- src/physics/RangeGenerator.cc
+++ src/physics/RangeGenerator.cc
@@ -101,12 +101,12 @@ void RangeGenerator::GenerateCommands() {
 
 //__Generate Initial Particles__________________________________________________________________
 void RangeGenerator::GeneratePrimaryVertex(G4Event* event) {
-  _particle.set_eta(G4MTRandFlat::shoot(_min.eta(), _max.eta()));
-  _particle.set_phi(G4MTRandFlat::shoot(_min.phi(), _max.phi()));
+  _particle.set_eta(G4RandFlat::shoot(_min.eta(), _max.eta()));
+  _particle.set_phi(G4RandFlat::shoot(_min.phi(), _max.phi()));
   if (_using_range_ke) {
-    _particle.set_ke(G4MTRandFlat::shoot(_min.ke(), _max.ke()));
+    _particle.set_ke(G4RandFlat::shoot(_min.ke(), _max.ke()));
   } else {
-    _particle.set_pT(G4MTRandFlat::shoot(_min.pT(), _max.pT()));
+    _particle.set_pT(G4RandFlat::shoot(_min.pT(), _max.pT()));
   }
   AddParticle(_particle, *event);
 }
