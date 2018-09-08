05e670ec7dfc9534c9ea9a8ea7ba9e08332a50db
bhgomes <bhgomes.github@gmail.com>
2018-09-08 10:13:45 -0400

    change header back

--- src/physics/RangeGenerator.cc
+++ src/physics/RangeGenerator.cc
@@ -21,7 +21,7 @@
 #include <ostream>
 
 #include <Geant4/Randomize.hh>
-#include <Geant4/G4MTHepRandom.hh>
+#include <Geant4/G4MTRandFlat.hh>
 #include <Geant4/tls.hh>
 
 #include "physics/Units.hh"
